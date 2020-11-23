cb1b55fb818b677205cd955b565e22eb5b4ffbcd
Ben <benjamin.main88@gmail.com>
2020-11-23 15:00:27 +0000

    Completed JSX Challenges - Day 1

---
+++ src/components/People.js
@@ -0,0 +1,11 @@
+const names = ["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"];
+
+const People = () => (
+    <ul>
+        {names.map((value, index) => (
+            <li key = { index }>{ value }</li>
+        ))}
+    </ul>
+)
+
+export default People;
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+const Square = () => (
+    <div style={{
+        background: "red",
+        width: 200,
+        height: 200,
+    }} />
+);
+
+export default Square;
