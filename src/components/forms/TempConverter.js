e8c867aeb3f7b9a216faeee565ffdaafbd314b42
Ben <benjamin.main88@gmail.com>
2020-11-27 19:08:54 +0000

    Update to TempConverter

--- src/components/forms/TempConverter.js
+++ src/components/forms/TempConverter.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 class TempConverter extends React.Component {
-
     constructor(props) {
-
         super(props);
 
         this.state = {
-            celcius: 0, 
+            celcius: 0,
             farenheit: 32,
         };
 
@@ -18,37 +16,44 @@ class TempConverter extends React.Component {
     handleChangeC(event) {
         const celcius = event.currentTarget.value; // get value from input (needs to be first otherwise using stale data)
 
-        this.setState({celcius: celcius}) // set state
-        
-        let farenheit = (celcius * 1.8) + 32; // formula
-        
-        this.setState({farenheit: farenheit}) // set other state
+        let farenheit = celcius * 1.8 + 32; // formula
+
+        this.setState({ farenheit: farenheit, celcius: celcius }); // set states
     }
 
     handleChangeF(event) {
         const farenheit = event.currentTarget.value;
 
-        this.setState({farenheit: farenheit})
-        
         let celcius = (farenheit - 32) / 1.8;
-        
-        this.setState({celcius: celcius})
+
+        this.setState({ celcius: celcius, farenheit: farenheit });
     }
 
     render() {
-
         const { celcius, farenheit } = this.state;
 
-        return(
+        return (
             <section class="jumbotron">
                 <form class="form-group">
                     <label for="celcius">Celcius</label>
-                    <input class="form-control" onChange={this.handleChangeC} value={ celcius } id="celcius" type="number"/>
+                    <input
+                        class="form-control"
+                        onChange={this.handleChangeC}
+                        value={celcius}
+                        id="celcius"
+                        type="number"
+                    />
                     <label for="farenheit">Farenheit</label>
-                    <input class="form-control" onChange={this.handleChangeF} value={ farenheit } id="farenheit" type="number"/>
+                    <input
+                        class="form-control"
+                        onChange={this.handleChangeF}
+                        value={farenheit}
+                        id="farenheit"
+                        type="number"
+                    />
                 </form>
             </section>
-        )
+        );
     }
 }
 
